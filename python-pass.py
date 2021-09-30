3a0b9988739aef2b4c79aba2b65e7e3958a5e285
ahmed-techno <ahmedabdkarem2@gmail.com>
2021-09-30 23:05:38 +0300

    Update python-pass.py
    Get the longest palindromic substring using python

--- python-pass.py
+++ python-pass.py
@@ -4,4 +4,10 @@ class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+        longest_str  = ''  
+        for i in range(len(s)): 
+            for j in range(len(s), i, -1):  
+                 s[i:j] == s[i:j][::-1]:
+                    longest_str = s[i:j]
+                    break
+        return longest_str
