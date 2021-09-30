ee7f8fa3c8131d6a7dc1a7a54e0b5a16930b6b38
ahmed-techno <ahmedabdkarem2@gmail.com>
2021-09-30 23:30:14 +0300

    Update python-pass.py
    To reduce time use the condition 
    len(longest_str) >= j-i:

--- python-pass.py
+++ python-pass.py
@@ -7,7 +7,9 @@ class Solution:
         longest_str  = ''  
         for i in range(len(s)): 
             for j in range(len(s), i, -1):  
-                 s[i:j] == s[i:j][::-1]:
+                if len(longest_str) >= j-i:   
+                    break
+                elif s[i:j] == s[i:j][::-1]:
                     longest_str = s[i:j]
                     break
         return longest_str
